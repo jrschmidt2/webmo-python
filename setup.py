48d5fa030926dcf14fcb30f48612730d34ba16ee
JR Schmidt <jrschmidt2@gmail.com>
2022-06-15 12:45:51 -0500

    Incremented version number to 0.2

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
   name = 'webmo',
   packages = ['webmo'],
-  version = '0.1',
+  version = '0.2',
   license='MIT',
   description = 'A Python-based interface to WebMO',
   author = 'J.R. Schmidt',
   author_email = 'schmidt@webmo.net',
   url = 'https://github.com/jrschmidt2/webmo-python',
-  download_url = 'https://github.com/jrschmidt2/webmo-python/archive/refs/tags/0.1.tar.gz',
+  download_url = 'https://github.com/jrschmidt2/webmo-python/archive/refs/tags/0.2.tar.gz',
   keywords = ['WEBMO', 'REST', 'API'],
   install_requires=[
           'requests',
