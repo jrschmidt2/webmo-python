e50984e46c6c4aaf4433de38d2a3433b8dd30fe1
JR Schmidt <schmidt@webmo.net>
2023-10-22 14:11:35 -0500

    Updated requirements

--- setup.py
+++ setup.py
@@ -8,9 +8,10 @@ setup(
   author = 'J.R. Schmidt',
   author_email = 'schmidt@webmo.net',
   url = 'https://github.com/jrschmidt2/webmo-python',
-  download_url = 'https://github.com/jrschmidt2/webmo-python/archive/refs/tags/1.0.2.tar.gz',
+  download_url = 'https://github.com/jrschmidt2/webmo-python/archive/refs/tags/1.1.0.tar.gz',
   keywords = ['WEBMO', 'REST', 'API'],
   install_requires=[
           'requests',
+          'websockets',
       ],
 )
