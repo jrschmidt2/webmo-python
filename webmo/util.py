07064210857e1f95092df03554199436e0aba557
unknown <JR Schmidt@schmidtpc.ad.wisc.edu>
2024-11-22 10:02:29 -0600

    Added utility functions for job results

--- webmo/util.py
+++ webmo/util.py
@@ -23,3 +23,98 @@ def xyz_from_name(name):
         geom += f"{element}\t{x:f}\t{y:f}\t{z:f}\n"
         
     return geom
+
+def get_energies(results, include_units=False):
+    """Returns a dictionary of available energies from the job results.
+
+    Arguments:
+        dict(results): A dictionary of job results, obtained from get_job_results.
+        bool(include_units): Whether to include the units with the returned energy (e.g. 1.5 vs. "1.5 Hartree")
+
+    Returns:
+        A dictionary of available energies.
+    """
+    properties = results['properties']
+    energies = {}
+
+    for key in properties.keys():
+        index = key.find('_energy')
+
+        if index >= 0:
+            energy_type = key[:index]
+
+            if include_units:
+                energies[energy_type] = f"{properties[key]['value']} {properties[key]['units']}"
+            else:
+                energies[energy_type] = properties[key]['value']
+
+    return energies
+
+def get_geometry(results):
+    """Returns the XYZ-formatted final geometry from the job results.
+
+    Arguments:
+        dict(results): A dictionary of job results, obtained from get_job_results.
+
+    Returns:
+        The XYZ-formatted final geometry as a string.
+    """
+
+
+    geom = ""
+    index = 0
+    for element in results['symbols']:
+
+        x = results['geometry'][index]
+        y = results['geometry'][index + 1]
+        z = results['geometry'][index + 2]
+
+        geom += f"{element}\t{x:f}\t{y:f}\t{z:f}\n"
+        index += 3
+
+    return geom
+
+def get_stoichiometry(results):
+    """Returns the stoichiometry from the job results.
+
+    Arguments:
+        dict(results): A dictionary of job results, obtained from get_job_results.
+
+    Returns:
+        The stoichiometry as a string.
+    """
+
+    properties = results['properties']
+    return properties['stoichiometry']
+
+def get_dipole_moment(results):
+    """Returns the dipole moment from the job results.
+
+    Arguments:
+        dict(results): A dictionary of job results, obtained from get_job_results.
+
+    Returns:
+        The magnitude of the dipole moment.
+    """
+
+    from math import sqrt
+
+    properties = results['properties']
+    dipole = properties['dipole_moment']
+    mag = sqrt(dipole[0]**2 + dipole[1]**2 + dipole[2]**2)
+
+    return mag
+
+def get_property(results, property_name):
+    """Returns an arbitrary calculated property from the job results.
+
+    Arguments:
+        dict(results): A dictionary of job results, obtained from get_job_results.
+        str(property_name): The name of the desired property
+
+    Returns:
+        The value of the calculated property (scalar or dictionary).
+    """
+
+    properties = results['properties']
+    return properties[property_name]
