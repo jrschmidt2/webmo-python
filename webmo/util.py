c03421953073bcc5ec9bbddc6538b9fee6608376
unknown <JR Schmidt@schmidtpc.ad.wisc.edu>
2025-02-14 15:49:02 -0600

    Added utility functions for bond length/angle/dihedral

--- webmo/util.py
+++ webmo/util.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 def xyz_from_name(name):
     """Obtains an XYZ-formatted geometry for the molecule name via PubChem lookup.
 
@@ -152,3 +154,104 @@ def get_property(results, property_name):
 
     properties = results['properties']
     return properties[property_name]
+
+
+def get_bond_length(results, index1, index2):
+    """Returns the specified bond length between two atoms from the job results.
+
+    Arguments:
+        dict(results): A dictionary of job results, obtained from get_job_results.
+        index1(int): The one-based index of the first atom of the bonded pair
+        index2(int): The one-based index of the second atom of the bonded pair
+
+    Returns:
+        The calculated bond length.
+    """
+
+    geom = results['geometry']
+
+    #convert to 0 index
+    index1 -= 1
+    index2 -= 1
+
+    atom1 = np.array(geom[index1*3:index1*3 + 3])
+    atom2 = np.array(geom[index2*3:index2*3 + 3])
+
+    return np.linalg.norm(atom1 - atom2)
+
+def get_bond_angle(results, index1, index2, index3):
+    """Returns the specified bond angle between three atoms from the job results.
+
+    Arguments:
+        dict(results): A dictionary of job results, obtained from get_job_results.
+        index1(int): The one-based index of the first atom
+        index2(int): The one-based index of the second atom
+        index3(int): The one-based index of the third atom
+
+    Returns:
+        The calculated bond angle.
+    """
+
+    geom = results['geometry']
+
+    #convert to 0 index
+    index1 -= 1
+    index2 -= 1
+    index3 -= 1
+
+    atom1 = np.array(geom[index1*3:index1*3 + 3])
+    atom2 = np.array(geom[index2*3:index2*3 + 3])
+    atom3 = np.array(geom[index3*3:index3*3 + 3])
+
+    vec1 = atom1 - atom2
+    vec2 = atom3 - atom2
+
+    vec1 /= np.linalg.norm(vec1)
+    vec2 /= np.linalg.norm(vec2)
+
+    return np.arccos(np.dot(vec1,vec2))*180./np.pi
+
+def get_dihedral_angle(results, index1, index2, index3, index4):
+    """Returns the specified dihedral angle between four atoms from the job results.
+
+    Arguments:
+        dict(results): A dictionary of job results, obtained from get_job_results.
+        index1(int): The one-based index of the first atom
+        index2(int): The one-based index of the second atom
+        index3(int): The one-based index of the third atom
+        index4(int): The one-based index of the fourth atom
+
+    Returns:
+        The calculated dihedral angle.
+    """
+
+    geom = results['geometry']
+
+    #convert to 0 index
+    index1 -= 1
+    index2 -= 1
+    index3 -= 1
+    index4 -= 1
+
+    atom1 = np.array(geom[index1*3:index1*3 + 3])
+    atom2 = np.array(geom[index2*3:index2*3 + 3])
+    atom3 = np.array(geom[index3*3:index3*3 + 3])
+    atom4 = np.array(geom[index4*3:index4*3 + 3])
+
+    b1 = atom1 - atom2
+    b2 = atom2 - atom3
+    b3 = atom3 - atom4
+
+    n1 = np.cross(b1, b2)
+    n2 = np.cross(b2, b3)
+    n3 = np.cross(n1, n2)
+
+    n1 /= np.linalg.norm(n1)
+    n2 /= np.linalg.norm(n2)
+    n3 /= np.linalg.norm(n3)
+
+    angle = np.arccos(np.dot(n1,n2))*180./np.pi
+    if np.dot(b2,n3) > 0:
+        angle = -angle
+
+    return angle
